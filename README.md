b84e0c1fef42f3e02d454ec79adf427e5fd822f0
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-11-19 00:12:37 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Simple-Web-Server
 Using Python to create an HTTP server.
+
+<h1>What is HTTP?</h1>
+HTTP is a stateless request/response protocol. To display a web page, the client sends a request to the server, which in turn sends the page back to the client. The client then displays the page.
+
+<h1>What is a web server?</h1>
+* A web server is a program that runs on a computer and responds to requests from web browsers of the client. 
+* The web server is responsible for receiving the request from the client, processing the request, and sending the response back to the client. * The web server is also responsible for maintaining the state of the server. For example, the web server can store the state of the server, such as the current date and time. 
+* The web server can also store the state of the client, such as the cookies that the client sends to the server.
+
+<h1>What is a port?</h1>
+Web server uses ports to listen for incoming requests. The port number is a number that the server uses to identify itself.
+
+<h1>Requirements</h1>
+
+* Python 3.6 or later
+
+<h1>Usage</h1>
+
+* To start the server, run the following command:
+
+```
+python3 server.py
+```
+
+* To specify html file, use the following command:
+
+```
+python3 server.py path/to/file.html
+```
+
+* To specify both port and html file, use the following command:
+
+```
+python3 server.py path/to/file.html port_number
+```
+
+* To stop the server, use the following key combination: <code>Ctrl + C</code>
---
+++ server.py
@@ -0,0 +1,63 @@
+"""
+A quick demonstration of a simple HTTP server 
+using the built-in Python libraries.
+"""
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from os import path
+from pathlib import Path
+import sys
+
+DEFAULT_PORT = 8110
+DEFAULT_HTML_PATH = "index.html"
+
+
+class SimpleWebServer(BaseHTTPRequestHandler):
+    """
+    A simple web server that serves html pages.
+    """
+    html_file_path = Path(__file__).parent.absolute() / DEFAULT_HTML_PATH
+
+    def do_GET(self) -> None:
+        """
+        Override the do_GET method to handle the GET requests
+        and send the file to the client if it exists. If the
+        file does not exist, send a 404 error.
+        """
+        try:
+            html_file_content = open(__class__.html_file_path).read()
+            self.send_response(200)
+        except:
+            html_file_content = "File not found"
+            self.send_response(404)
+
+        self.end_headers()
+        self.wfile.write(bytes(html_file_content, 'utf-8'))
+
+
+if __name__ == '__main__':
+    
+    port = DEFAULT_PORT
+
+    if len(sys.argv) == 1:
+        print(f'Using default html file path: {DEFAULT_HTML_PATH} and a default port: {DEFAULT_PORT}')
+
+    elif len(sys.argv) == 2 or len(sys.argv) == 3:
+        html_file_path = sys.argv[1]
+        if not path.exists(html_file_path):
+            print(f'File {html_file_path} does not exist')
+            exit()
+
+        if len(sys.argv) == 3:
+            port = int(sys.argv[2])
+            if port < 0 or port > 65535:
+                print(f'Port {port} is not valid')
+                exit()
+    
+    else:
+        print('Usage: python server.py [html_file_path] [port]')
+        exit()
+
+    httpd = HTTPServer(('localhost', port), SimpleWebServer)
+    httpd.RequestHandlerClass.html_file_path = html_file_path
+    httpd.serve_forever()
+    print(f'Serving HTTP on port {port}...')
